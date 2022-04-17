a1a1b4aedafec6ec7c63014aeac1d5045c18acd7
Sobir <sobirjon001@gmail.com>
2022-04-16 19:24:26 -0500

    updated endpoints

--- api/users_microservice/users.router.js
+++ api/users_microservice/users.router.js
@@ -40,27 +40,19 @@ router.get("/role/:id", get_role_by_role_id);
 router.get("/account/:id", get_account_info_by_account_id);
 router.get("/role_names_by_user_id/:id", get_role_names_by_user_id);
 router.get("/:id", get_user_by_id);
-router.post("/create", validateSchema("new-user"), create_user);
+router.post("/", validateSchema("new-user"), create_user);
 router.post("/protected", set_protected_users);
-router.post("/create_role", validateSchema("new-role"), create_role);
+router.post("/role", validateSchema("new-role"), create_role);
 router.post("/protected_roles", set_protected_roles);
-router.post(
-  "/create_account",
-  validateSchema("create-account"),
-  create_account
-);
+router.post("/account", validateSchema("create-account"), create_account);
 router.post("/sign_up", validateSchema("sign-up"), student_employee_sign_up);
-router.patch("/update", validateSchema("update-user"), update_user);
-router.patch("/update_role", validateSchema("update-role"), update_role);
+router.patch("/", validateSchema("update-user"), update_user);
+router.patch("/role", validateSchema("update-role"), update_role);
 router.patch("/protected", remove_user_protection);
 router.patch("/protected_roles", remove_role_protection);
-router.patch(
-  "/update_account",
-  validateSchema("update-account"),
-  update_account
-);
+router.patch("/account", validateSchema("update-account"), update_account);
 router.delete("/", delete_users_by_user_ids);
-router.delete("/delete_roles", delete_roles_by_role_ids);
+router.delete("/roles", delete_roles_by_role_ids);
 router.delete("/accounts_by_ids", delete_accounts_by_account_ids);
 
 module.exports = router;
